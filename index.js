042eba197d7b59f911ac5f111644ddc04c4b08b3
kd00009 <disojakaran@gmail.com>
2023-07-05 19:13:15 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const morgan = require("morgan");
 const app = express();
 const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 const connectdb = require("./config/db.js");
-
+app.use(cors());
 dotenv.config();
 
 // router import
